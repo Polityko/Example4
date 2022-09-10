730049c0e931563f61eac84a1e2ba8c13138460d
Olga <politykoolga@outlook.com>
2022-09-10 20:56:33 +0300

    First commit

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение степени (число B), в которую будет возводиться число А: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int power = A;
+
+for (int i = 1; i < B; i++)
+{
+    power = power * A;
+}
+
+Console.WriteLine("A в степени B равно " + power);
